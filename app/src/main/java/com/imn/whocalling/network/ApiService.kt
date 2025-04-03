9940f68210a969664028f2e4b0031de394a556ae
u178171 <imnithish@live.com>
2025-04-03 16:12:12 +0530

    Update Readme

--- app/src/main/java/com/imn/whocalling/network/ApiService.kt
+++ app/src/main/java/com/imn/whocalling/network/ApiService.kt
@@ -1,11 +1,11 @@
 package com.imn.whocalling.network
 
-import com.imn.whocalling.MockyName
 import com.imn.whocalling.data.MockyResponse
+import com.imn.whocalling.util.Constants
 import retrofit2.http.GET
 import retrofit2.Response
 
 interface ApiService {
-    @GET("v3/$MockyName")
+    @GET("v3/${Constants.MOCKY_NAME}")
     suspend fun getMockyData(): Response<MockyResponse>
 }
---
+++ app/src/main/java/com/imn/whocalling/util/Constants.kt
@@ -0,0 +1,35 @@
+package com.imn.whocalling.util
+
+object Constants {
+    const val MOCKY_NAME ="4ca5343d-256f-4325-8e46-04bbb493ae52"
+
+//{
+//    "numbers": [
+//    {
+//        "number": "+918907471155",
+//        "name": "Nitheesh",
+//        "isSpam": false,
+//        "location": "Trivandrum"
+//    },
+//    {
+//        "number": "+919876543210",
+//        "name": "John Doe",
+//        "isSpam": false,
+//        "location": "Mumbai, India"
+//    },
+//    {
+//        "number": "+441234567890",
+//        "name": "Spam Caller",
+//        "isSpam": true,
+//        "location": "London, UK"
+//    },
+//    {
+//        "number": "+15551234567",
+//        "name": "Alice Smith",
+//        "isSpam": false,
+//        "location": "New York, USA"
+//    }
+//    ]
+//}
+//https://designer.mocky.io/
+}
